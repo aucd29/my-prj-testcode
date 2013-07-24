a25c58e4686c545472b1a6dc7e2dd6d157b90bc5
aucd29 <aucd29@gmail.com>
2013-07-24 10:48:20 +0900

    add project for upload  (http multipart)

---
+++ android/test-prj/FileUpload/src/com/example/fileupload/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.fileupload;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+    private static final String TAG = "MainActivity";
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.upload);
+        btn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        HttpClient httpclient = new DefaultHttpClient();
+                        HttpPost httppost = new HttpPost("http://www.sarangnamu.net/test/upload.php");
+
+                        try {
+                            MultipartEntity entity = new MultipartEntity();
+                            entity.addPart("uploadFile", new FileBody(new File("/mnt/sdcard/bg_add_ac.png")));
+                            httppost.setEntity(entity);
+                            HttpResponse response = httpclient.execute(httppost);
+
+                            Log.d(TAG, EntityUtils.toString(response.getEntity()));
+                        } catch (ClientProtocolException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+}
