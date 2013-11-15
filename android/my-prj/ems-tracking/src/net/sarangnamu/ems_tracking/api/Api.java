6399f8e00c83adef4f0fa77e600440084febf256
aucd29 <aucd29@gmail.com>
2013-11-15 14:55:04 +0900

    - ems-tracking > api 관련 class 추가

--- android/my-prj/ems-tracking/src/net/sarangnamu/ems_tracking/api/Api.java
+++ android/my-prj/ems-tracking/src/net/sarangnamu/ems_tracking/api/Api.java
@@ -17,34 +17,14 @@
  */
 package net.sarangnamu.ems_tracking.api;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import net.sarangnamu.common.DLog;
-import net.sarangnamu.common.json.JsonTool;
 import net.sarangnamu.common.network.BkHttp;
-import net.sarangnamu.home.api.json.Notice;
-import net.sarangnamu.home.api.json.Study;
-
-import org.apache.http.cookie.Cookie;
-import org.json.JSONObject;
-
-import com.fasterxml.jackson.core.type.TypeReference;
 
 public class Api {
     private static final String TAG = "Api";
 
-    public static final String URL = "http://www.sarangnamu.net/api";
-    public static final String URI_NOTICE       = URL + "/notice.php";
-    public static final String URI_NOTICE_WRITE = URL + "/noticeWrite.php";
-    public static final String URI_STUDY        = URL + "/study.php";
-    public static final String URI_CATEGORIES   = URL + "/categories.php";
-    public static final String URI_LOGIN        = URL + "/login.php";
+    public static final String URL = "http://smart.epost.go.kr:8080/servlet/kpl.vis.common.svl.VisSVL";
 
     private static BkHttp http;
-    public static String userId;
 
     private static void initHttp() {
         if (http == null) {
@@ -52,75 +32,79 @@ public class Api {
         }
     }
 
-    public static ArrayList<Notice> notices(int page) throws Exception {
-        initHttp();
-        http.setMethod("GET");
+    public static void tracking(String num) {
 
-        Map<String, String> params = new HashMap<String, String>();
-        params.put("nPage", "" + page);
-        String res = http.submit(URI_NOTICE, params);
-
-        return (ArrayList<Notice>) JsonTool.toObj(res, new TypeReference<List<Notice>>(){});
     }
 
-    public static boolean noticeWrite(String msg) throws Exception {
-        initHttp();
-        http.setMethod("POST");
-
-        Map<String, String> params = new HashMap<String, String>();
-        params.put("sContent", msg);
-        String res = http.submit(URI_NOTICE_WRITE, params);
-
-        return true;
-    }
-
-    public static ArrayList<Study> study(int page) throws Exception {
-        initHttp();
-        http.setMethod("GET");
-
-        Map<String, String> params = new HashMap<String, String>();
-        params.put("nPage", "" + page);
-        String res = http.submit(URI_STUDY, params);
-
-        return (ArrayList<Study>) JsonTool.toObj(res, new TypeReference<List<Study>>(){});
-    }
-
-    public static void categories() throws Exception {
-        initHttp();
-        http.setMethod("GET");
-
-        String res = http.submit(URI_CATEGORIES, null);
-        JSONObject obj = new JSONObject(res);
-    }
-
-    public static boolean login(String id, String pw) throws Exception {
-        initHttp();
-        http.setMethod("POST");
-
-        Map<String, String> params = new HashMap<String, String>();
-        params.put("id", id);
-        params.put("passwd", pw);
-        params.put("rnd", "pre-" + System.currentTimeMillis() / 1000);
-
-        String res = http.submit(URI_LOGIN, params);
-
-        JSONObject json = new JSONObject(res);
-        String login = "";
-
-        try {
-            login = json.getString("res");
-        } catch (Exception e) {
-            DLog.e(TAG, "login", e);
-        }
-
-        List<Cookie> cookies = http.getCookie();
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("sMemberID")) {
-                userId = cookie.getName();
-            }
-        }
-
-        return login.equals("success") && userId != null;
-    }
+    //    public static ArrayList<Notice> notices(int page) throws Exception {
+    //        initHttp();
+    //        http.setMethod("GET");
+    //
+    //        Map<String, String> params = new HashMap<String, String>();
+    //        params.put("nPage", "" + page);
+    //        String res = http.submit(URI_NOTICE, params);
+    //
+    //        return (ArrayList<Notice>) JsonTool.toObj(res, new TypeReference<List<Notice>>(){});
+    //    }
+    //
+    //    public static boolean noticeWrite(String msg) throws Exception {
+    //        initHttp();
+    //        http.setMethod("POST");
+    //
+    //        Map<String, String> params = new HashMap<String, String>();
+    //        params.put("sContent", msg);
+    //        String res = http.submit(URI_NOTICE_WRITE, params);
+    //
+    //        return true;
+    //    }
+    //
+    //    public static ArrayList<Study> study(int page) throws Exception {
+    //        initHttp();
+    //        http.setMethod("GET");
+    //
+    //        Map<String, String> params = new HashMap<String, String>();
+    //        params.put("nPage", "" + page);
+    //        String res = http.submit(URI_STUDY, params);
+    //
+    //        return (ArrayList<Study>) JsonTool.toObj(res, new TypeReference<List<Study>>(){});
+    //    }
+    //
+    //    public static void categories() throws Exception {
+    //        initHttp();
+    //        http.setMethod("GET");
+    //
+    //        String res = http.submit(URI_CATEGORIES, null);
+    //        JSONObject obj = new JSONObject(res);
+    //    }
+    //
+    //    public static boolean login(String id, String pw) throws Exception {
+    //        initHttp();
+    //        http.setMethod("POST");
+    //
+    //        Map<String, String> params = new HashMap<String, String>();
+    //        params.put("id", id);
+    //        params.put("passwd", pw);
+    //        params.put("rnd", "pre-" + System.currentTimeMillis() / 1000);
+    //
+    //        String res = http.submit(URI_LOGIN, params);
+    //
+    //        JSONObject json = new JSONObject(res);
+    //        String login = "";
+    //
+    //        try {
+    //            login = json.getString("res");
+    //        } catch (Exception e) {
+    //            DLog.e(TAG, "login", e);
+    //        }
+    //
+    //        List<Cookie> cookies = http.getCookie();
+    //        for (Cookie cookie : cookies) {
+    //            if (cookie.getName().equals("sMemberID")) {
+    //                userId = cookie.getName();
+    //            }
+    //        }
+    //
+    //        return login.equals("success") && userId != null;
+    //    }
 
 }
