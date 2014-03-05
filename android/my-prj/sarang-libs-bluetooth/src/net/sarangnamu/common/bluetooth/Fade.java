7b8450a96147d3e8269989c79fd40acb888a06dc
obigo <obigo@obigo-H61H2-MV.(none)>
2014-03-05 10:15:51 +0900

    new file:   sarang-libs-bluetooth/.classpath 	new file:   sarang-libs-bluetooth/.project 	new file:   sarang-libs-bluetooth/AndroidManifest.xml 	new file:   sarang-libs-bluetooth/proguard-project.txt 	new file:   sarang-libs-bluetooth/project.properties 	new file:   sarang-libs-bluetooth/src/net/sarangnamu/common/bluetooth/Fade.java 	new file:   sarang-libs-bluetooth/src/net/sarangnamu/common/bluetooth/FadeColor.java 	new file:   sarang-libs-bluetooth/src/net/sarangnamu/common/bluetooth/Transition.java 	  > add project for file transfer by bluetooth

---
+++ android/my-prj/sarang-libs-bluetooth/src/net/sarangnamu/common/bluetooth/Fade.java
@@ -0,0 +1,50 @@
+/*
+ * ChangeFade.java
+ * Copyright 2013 Burke Choi All rights reserved.
+ *             http://www.sarangnamu.net
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.sarangnamu.common.ani;
+
+import android.animation.Animator.AnimatorListener;
+import android.animation.ObjectAnimator;
+import android.view.View;
+
+/**
+ * {@code
+ * <pre>
+    Fade.start(hideView, showView, null);
+ * </pre>}
+ * 
+ * @author <a href="mailto:aucd29@gmail.com.com">Burke Choi</a>
+ */
+public class Fade {
+    private static final String TAG = "Fade";
+
+    public static void start(View hideView, View showView, AnimatorListener l) {
+        if (hideView == null || showView == null) {
+            return ;
+        }
+
+        ObjectAnimator.ofFloat(hideView, "alpha", 0.25f, 1, 1).start();
+        ObjectAnimator obj = ObjectAnimator.ofFloat(showView, "alpha", 1f, 1, 1);
+
+        if (l != null) {
+            obj.addListener(l);
+        }
+
+        obj.start();
+    }
+}
+
