a1689d51343f19e1419f8a6747edd546b2b16406
obigo <obigo@obigo-H61H2-MV.(none)>
2014-03-05 15:31:01 +0900

    php  remove new dir

--- php/new/application/controllers/profile.php
+++
@@ -1,9 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-class Profile extends CI_Controller {
-
-	public function index()
-	{
-		$this->load->view('profile');
-	}
-}
--- php/new/application/controllers/study.php
+++
@@ -1,9 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-class Study extends CI_Controller {
-
-	public function index()
-	{
-		$this->load->view('study');
-	}
-}
