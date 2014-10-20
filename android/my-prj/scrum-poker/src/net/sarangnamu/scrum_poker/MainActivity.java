53f3a907bd1fdc3974c42c1a6a9b516ea03c8c1b
aucd29 <aucd29@gmail.com>
2014-10-20 18:21:45 +0900

    libs-common > add android-support-v4 and v7 jar files
    libs-fragment
    > remove android-support-v4
    
    scrum-poker
    > add ui

--- android/my-prj/scrum-poker/src/net/sarangnamu/scrum_poker/MainActivity.java
+++ android/my-prj/scrum-poker/src/net/sarangnamu/scrum_poker/MainActivity.java
@@ -1,37 +1,42 @@
 package net.sarangnamu.scrum_poker;
 
-import org.apache.http.client.methods.HttpGet;
+import java.util.ArrayList;
 
 import android.app.Activity;
-import android.content.Context;
-import android.net.http.AndroidHttpClient;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.DrawerLayout.DrawerListener;
+import android.view.LayoutInflater;
 import android.view.Menu;
-
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
     private static final String TAG = "MainActivity";
+    private GridView grid;
+    private ListView leftMenu;
+    private DrawerLayout drawer;
+    private ArrayList<String> defaultValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new AsyncTask<Context, Void, String>() {
-            @Override
-            protected String doInBackground(Context... contexts) {
-                AndroidHttpClient client = AndroidHttpClient.newInstance("Android");
-                HttpGet request = new HttpGet("http://117.16.231.212:8010/checkNewOrder");
+        grid = (GridView) findViewById(R.id.grid);
+        leftMenu = (ListView) findViewById(R.id.leftMenu);
+        drawer = (DrawerLayout) findViewById(R.id.drawer);
 
-                return builder.toString();
-            }
-
-            @Override
-            protected void onPostExecute(String result) {
-
-            }
-        }.execute(this);
+        initDefaultValue();
+        initDrawer();
+        initActionBar();
+        initAdapter();
     }
 
     @Override
@@ -39,28 +44,164 @@ public class MainActivity extends Activity {
         return true;
     }
 
-    /*@Override
-    public boolean onMenuItemSelected(int featureId, MenuItem item) {
-        switch (item.getItemId()) {
-        case R.id.action_license:
-            DlgLicense dlg = new DlgLicense(MainActivity.this);
-            dlg.setTitleTypeface(FontLoader.getInstance(getApplicationContext()).getRobotoLight());
-            dlg.show();
-            break;
-
-        case R.id.action_settings:
-            break;
+    private void initDefaultValue() {
+        if (defaultValue == null) {
+            defaultValue = new ArrayList<String>();
         }
 
-        return super.onMenuItemSelected(featureId, item);
+        defaultValue.add("0");
+        defaultValue.add("1/2");
+        defaultValue.add("2");
+        defaultValue.add("3");
+        defaultValue.add("5");
+        defaultValue.add("8");
+        defaultValue.add("13");
+        defaultValue.add("20");
+        defaultValue.add("40");
+        defaultValue.add("50");
+        defaultValue.add("100");
     }
 
-    public void showPopup(String msg) {
-        DlgTimer dlg = new DlgTimer(MainActivity.this, R.layout.dlg_timer);
-        dlg.setMessage(msg);
-        dlg.setTime(1000);
-        dlg.show();
-        dlg.setTransparentBaseLayout();
-    }*/
-}
+    private void initActionBar() {
 
+    }
+
+    private void initDrawer() {
+        drawer.setDrawerListener(new DrawerListener() {
+            @Override
+            public void onDrawerStateChanged(int arg0) {
+            }
+
+            @Override
+            public void onDrawerSlide(View arg0, float arg1) {
+            }
+
+            @Override
+            public void onDrawerOpened(View arg0) {
+            }
+
+            @Override
+            public void onDrawerClosed(View arg0) {
+            }
+        });
+    }
+
+    private void initAdapter() {
+        grid.setAdapter(new ScrumAdapter());
+        grid.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
+        leftMenu.setAdapter(new MenuAdapter());
+        leftMenu.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+            }
+        });
+    }
+
+    // //////////////////////////////////////////////////////////////////////////////////
+    //
+    // SCRUM ADAPTER
+    //
+    // //////////////////////////////////////////////////////////////////////////////////
+
+    class ViewHolder {
+        TextView number;
+    }
+
+    class ScrumAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            if (defaultValue == null) {
+                return 0;
+            }
+
+            return defaultValue.size();
+        }
+
+        @Override
+        public Object getItem(int arg0) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int arg0) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.scrum_item, null);
+
+                holder = new ViewHolder();
+                holder.number = (TextView) convertView.findViewById(R.id.number);
+
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            holder.number.setText(defaultValue.get(position));
+
+            return convertView;
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    //
+    // MENU ADAPTER
+    //
+    ////////////////////////////////////////////////////////////////////////////////////
+
+    class MenuViewHolder {
+        TextView menu;
+    }
+
+    class MenuAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int arg0) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int arg0) {
+            return 0;
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            return super.getItemViewType(position);
+        }
+
+        @Override
+        public View getView(int arg0, View convertView, ViewGroup arg2) {
+            MenuViewHolder holder;
+
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.scrum_item, null);
+
+                holder = new MenuViewHolder();
+
+                convertView.setTag(holder);
+            } else {
+                holder = (MenuViewHolder) convertView.getTag();
+            }
+
+
+            return convertView;
+        }
+    }
+}
